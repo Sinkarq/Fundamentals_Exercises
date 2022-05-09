f56a92623b2d3d88a3ff9465a1f206eb2bd1af83
Sinan <ebazier@outlook.com>
2022-05-09 18:41:09 +0300

    MODIFIED Objects and Composition - Exercise/04. Heroic Inventory.js SO IT CAN PASS THE JUDGE CORRECTLY

--- Advanced  JS Exercise/Objects and Composition - Exercise/04. Heroic Inventory.js
+++ Advanced  JS Exercise/Objects and Composition - Exercise/04. Heroic Inventory.js
@@ -4,17 +4,17 @@ function Solve(array) {
     for (const hero of array) {
         let [name, level, items] = hero.split(" / ");
         let itemsStorage = [];
-        const itemsArray = items.split(", ");
+        const itemsArray = items ? items.split(", ") : [];
         for (const item of itemsArray) {
             itemsStorage.push(item);
         }
-        level = level.toString();
-        heroes.push({name: name, level: level, items: itemsStorage});
+        level = Number(level);
+        heroes.push({name, level, items: itemsStorage});
     }
 
-    return JSON.stringify(heroes);
+    console.log(JSON.stringify(heroes));
 }
 
-console.log(Solve(['Isacc / 25 / Apple, GravityGun',
+Solve(['Isacc / 25 / Apple, GravityGun',
     'Derek / 12 / BarrelVest, DestructionSword',
-    'Hes / 1 / Desolator, Sentinel, Antara']));
+    'Hes / 1 / Desolator, Sentinel, Antara']);
