cf30d2e4a21fb5b23cdc88f002078a44ca54e25d
Sinkarq <ebazier@outlook.com>
2022-09-27 22:28:28 +0300

    Added first React App

---
+++ React/cool-app/src/components/Logos/ReactLogo.jsx
@@ -0,0 +1,9 @@
+import reactLogo from "../../assets/react.svg";
+
+export default function ViteLogo() {
+    return (
+        <a href="https://reactjs.org" target="_blank">
+            <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+    );
+};
---
+++ React/cool-app/src/components/Logos/ViteLogo.jsx
@@ -0,0 +1,7 @@
+export default function ViteLogo() {
+    return (
+        <a href="https://vitejs.dev" target="_blank">
+            <img src="/vite.svg" className="logo" alt="Vite logo"/>
+        </a>
+    );
+};
